ca0dcae1bd7cab0309377b7b84a6892630dec5bc
Shake564 <hdrake237@gmail.com>
2025-12-13 05:13:06 +0000

    finding errors in databaseSetup

--- src/database/databaseAggregateFunctions.cjs
+++ src/database/databaseAggregateFunctions.cjs
@@ -8,7 +8,7 @@ class databaseClass {
         this.db = new sqlite3.Database(dbPath, (err) => {
             if (err) 
                 console.error(err.message);
-            console.log('Connected to the SQLite database.');
+            console.log(`Connected to the SQLite database at: ${dbPath}`);
         });
     }
 
