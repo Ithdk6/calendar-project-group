9fe0d67fbef6f05c0a06fde311507c85118def27
Shake564 <hdrake237@gmail.com>
2025-12-13 03:57:23 +0000

    new file path test

--- src/database/databaseSetup.js
+++ src/database/databaseSetup.js
@@ -61,7 +61,7 @@ async function executeSetup() {
 
 // read the sql file content
 async function readFileContent() {
-    const filePath = path.resolve(__dirname, '../../Calendar.sql');
+    const filePath = path.resolve(__dirname, 'src/database/Calendar.sql');
     try {
         const data = await fs.readFile(filePath, 'utf8');
         
@@ -76,4 +76,3 @@ async function readFileContent() {
     }
 }
 executeSetup();
-// 3. Close the connection when done
