e3310a262fbc10ee217d91ad0137d1b1d8284ee4
Shake564 <hdrake237@gmail.com>
2025-12-13 03:39:08 +0000

    test

--- src/database/databaseSetup.js
+++ src/database/databaseSetup.js
@@ -3,38 +3,52 @@ const fs = require('fs').promises;
 
 //Connect to the database
 //if database doesn't exist, it will be created
-db = new sqlite3.Database(process.env.DB_NAME || 'calendar', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the SQLite database.');
-});
+
 
 async function executeSetup() {
-    sqlSetup = await readFileContent();
-    await new Promise((resolve, reject) => {
-    db.exec(sqlSetup, (err) => {
-        if (err) {
-            console.error("Error executing SQL setup:", err.message);
-            reject(err);
-        }
-        else{
-            console.log("Database setup completed successfully.");
-            resolve();
-        }
-    });
-    await new Promise((resolve, reject) =>{
-        db.close((err) => {
-            if (err) {
-                console.error(err.message);
-                reject(err);
-            }
-            else{
-                console.log('Close the database connection.');
-                resolve();
-            }
+    try{
+        const sqlSetup = await readFileContent();
+        
+        const db = await new Promise((resolve, reject) => {
+            const connection = new sqlite3.Database(process.env.DB_NAME || 'calendar', (err) => {
+                if (err) {
+                    console.error("Connection error:", err.message);
+                    return reject(err);
+                }
+                console.log(`Connected to the SQLite database at: ${dbPath}`);
+                resolve(connection);
+            });
         });
-    });
+
+        await new Promise((resolve, reject) => {
+            db.exec(sqlSetup, (err) => {
+                if (err) {
+                    console.error("Error executing SQL setup:", err.message);
+                    return reject(err);
+                }
+                else{
+                    console.log("Database setup completed successfully.");
+                    resolve();
+                }
+            });
+        });
+        await new Promise((resolve, reject) =>{
+            db.close((err) => {
+               if (err) {
+                   console.error(err.message);
+                   return reject(err);
+                }
+               else{
+                   console.log('Close the database connection.');
+                   resolve();
+               }
+            });
+        });
+    }
+    catch (error) {
+        console.error("Setup failed:", error);
+        process.exit(1);
+    }
 }
 
 // read the sql file content
